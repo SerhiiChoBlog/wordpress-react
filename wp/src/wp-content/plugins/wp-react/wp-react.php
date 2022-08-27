9e088a4cdce56e86abe93a663e3917f2d70edd26
SerhiiCho <serhiicho@protonmail.com>
2022-08-27 10:28:25 +0300

    Temporary removed content of the wp-react.php file

--- wp/src/wp-content/plugins/wp-react/wp-react.php
+++ wp/src/wp-content/plugins/wp-react/wp-react.php
@@ -7,7 +7,7 @@ Plugin Name: WP React
 Author: Serhii Cho
 Author URI: https://serhii.io
 Description: Test plugin for showing how to make an admin panel with React JS and TypeScript
-Version: 1.0.0
+Version: 0.1
 License: no
 License URI: https://serhii.io
 Text Domain: wpreact
@@ -17,9 +17,3 @@ Tags: custom-background, custom-logo, custom-menu, featured-images, threaded-com
 defined('ABSPATH') || exit('ABSPATH is not defined');
 define('WPREACT_PATH', plugin_dir_path(__FILE__));
 define('WPREACT_URL', plugin_dir_url(__FILE__));
-
-try {
-    (new Hook())->init();
-} catch (Throwable $e) {
-    error_log($e->getMessage());
-}
